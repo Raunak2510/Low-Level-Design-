40605df3f0d1a8d561b768e444f84a2fa7f125f1
RaunakCipherSchools <raunak.cipherschools@gmail.com>
2025-03-24 21:49:50 +0530

    learn solid principles

---
+++ Oops /class-object.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyClass {
+    public:
+    int myNum;
+   // string myString;
+};
+
+int main(){
+    MyClass myObj;
+    myObj.myNum=15;
+    cout<<myObj.myNum<<endl;
+}
